02e50f81804edbbcc18ebe333590d77306725c94
Marko Vidaković <markovidakovic29@gmail.com>
2023-06-01 10:57:55 +0200

    -

--- main.go
+++ main.go
@@ -1,4 +1,60 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type WebsiteInfo struct {
+	URL   string
+	Title string
+}
+
 func main() {
+	var urls []string = []string{
+		"https://links.hr",
+		"https://instar-informatika.hr",
+	}
+
+	// channel that receives website info
+	results := make(chan WebsiteInfo)
+
+	// start goroutines to scrape websites concurrently
+	for _, url := range urls {
+		go func(u string) {
+			wi, err := scrapeWebsite(u)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			results <- wi
+		}(url)
+	}
+
+	// wait for all goroutines to finish
+	for i := 0; i < len(urls); i++ {
+		info := <-results
+		fmt.Printf("URL: %s\nTitle: %s\n", info.URL, info.Title)
+	}
+}
+
+func scrapeWebsite(url string) (WebsiteInfo, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return WebsiteInfo{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return WebsiteInfo{}, err
+	}
+	fmt.Println(body)
+
+	info := WebsiteInfo{
+		URL:   url,
+		Title: "website title",
+	}
+	return info, nil
 }
